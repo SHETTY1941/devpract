b800e3a4ab78ef37f0fd9682c3fb78669e151a59
SHETTY1941 <shettybhavanivaraprasad@gmail.com>
2026-03-26 11:38:16 +0530

    updaed

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
-public Class sum{
-	public static void main(String[] args){
-		int x = 5,y=10;
-		System.out.println("x+y = "+ x+y);
-	}
+public class Sum {
+    public static void main(String[] args) {
+        int x = 5, y = 10;
+        System.out.println("x+y = " + (x + y));
+    }
 }
